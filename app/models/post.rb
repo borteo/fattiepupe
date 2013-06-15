22dfadb11c27fae7b710acba5f08695c3a79d3c6
Matteo Borgato <borgato.matteo@gmail.com>
2013-06-15 10:53:57 -0300

    upload image to S3

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,16 +8,16 @@ class Post
 
   has_mongoid_attached_file :image,
     :storage        => :s3,
-    :path           => ':image/:id/:style.:extension',
-    :bucket         => 'borteo',
+    :path           => 'images/:id/:style.:extension',
     :url            => ':s3_alias_url',
+    :bucket         => ENV['S3_BUCKET'],
     :s3_credentials => {
       :access_key_id      => ENV['S3_ACCESS_KEY'],
       :secret_access_key  => ENV['S3_SECRET_KEY']
     },
-    :styles => {
-      :original => ['640x720>', :jpg],
-      :small    => ['240x320#',   :jpg],
+    :styles         => {
+      :original           => ['640x720>', :png],
+      :small              => ['240x320#', :png],
     }
 
 end
